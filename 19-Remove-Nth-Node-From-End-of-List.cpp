4ee406ce8d2b5193039fe96b583117441ba26ef2
Arnav Jain <106018413+arnav-jain700@users.noreply.github.com>
2026-08-17 19:51:14 +0530

    [19] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 19-Remove-Nth-Node-From-End-of-List.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+        
+        for (int i = 0; i <= n; ++i) {
+            fast = fast->next;
+        }
+        
+        while (fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        ListNode* nodeToDelete = slow->next;
+        slow->next = slow->next->next;
+        
+        delete nodeToDelete;
+        
+        ListNode* newHead = dummy->next;
+        delete dummy;
+        
+        return newHead;
+    }
+};
