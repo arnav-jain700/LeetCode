d63eae69a1d69bc16de9f707687f8dfbf7799199
Arnav Jain <106018413+arnav-jain700@users.noreply.github.com>
2026-08-19 10:15:41 +0530

    [102] [Time Beats: -%] [Memory Beats: -%] - LeetPush

---
+++ 102-Binary-Tree-Level-Order-Traversal.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        
+        if (root == nullptr) {
+            return result;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> currentLevel;
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* currentNode = q.front();
+                q.pop();
+                
+                currentLevel.push_back(currentNode->val);
+                
+                if (currentNode->left != nullptr) {
+                    q.push(currentNode->left);
+                }
+                if (currentNode->right != nullptr) {
+                    q.push(currentNode->right);
+                }
+            }
+            
+            result.push_back(currentLevel);
+        }
+        
+        return result;
+    }
+};
