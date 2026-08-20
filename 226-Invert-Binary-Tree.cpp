fbe2ed4326bd8345d496d5a7261fbfc4503cb8a9
Arnav Jain <106018413+arnav-jain700@users.noreply.github.com>
2026-08-20 21:24:33 +0530

    [226] [Time Beats: 100.00%] [Memory Beats: 26.47%] - LeetPush

---
+++ 226-Invert-Binary-Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        
+        TreeNode* invertedLeft = invertTree(root->left);
+        TreeNode* invertedRight = invertTree(root->right);
+        
+        root->left = invertedRight;
+        root->right = invertedLeft;
+        
+        return root;
+    }
+};
