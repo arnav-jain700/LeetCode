5a10623f5f0bfcae966ce0a70b9a5a2523a2d9d1
Arnav Jain <106018413+arnav-jain700@users.noreply.github.com>
2026-08-21 22:23:06 +0530

    [136] [Time Beats: 24.56%] [Memory Beats: 59.07%] - LeetPush

---
+++ 136-Single-Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int uniqueNumber = 0;
+        
+        for (int num : nums) {
+            uniqueNumber ^= num;
+        }
+        
+        return uniqueNumber;
+    }
+};
