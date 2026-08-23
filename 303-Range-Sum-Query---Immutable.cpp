fa2cf3ad7a3a26d337b625cb898c93e9ae27728a
Arnav Jain <106018413+arnav-jain700@users.noreply.github.com>
2026-08-23 18:54:11 +0530

    [303] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 303-Range-Sum-Query---Immutable.cpp
@@ -0,0 +1,18 @@
+class NumArray {
+private:
+    vector<int> prefixSums;
+
+public:
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        prefixSums.resize(n + 1, 0);
+        
+        for (int i = 0; i < n; i++) {
+            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return prefixSums[right + 1] - prefixSums[left];
+    }
+};
